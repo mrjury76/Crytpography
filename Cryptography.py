4ff92bb9b3f2571de77bc00be246fc14567c133f
mrjury76 <mrjury76@gmail.com>
2025-03-21 01:39:02 -0700

    added documentation

--- Cryptography.py
+++ Cryptography.py
@@ -10,26 +10,33 @@ class Cryptography:
         return get_random_bytes(32)
 
     def encrypt_aes(self, key, plaintext):
+        #create random 16 byte Initialization Vector
         iv = get_random_bytes(16)
         cipher = AES.new(key, AES.MODE_CBC, iv)
 
+        #AES requires the text to be a length of 16 so this adds some padding
         padding_length = 16 - (len(plaintext) % 16)
         padded_text = plaintext + (chr(padding_length) * padding_length)
 
         ciphertext = cipher.encrypt(padded_text.encode())
+
+        #return the ciphertext with the IV concatenated in front all in byte format
         return base64.b64encode(iv + ciphertext).decode()
 
     def decrypt_aes(self, key, encrypted_text):
         encrypted_data = base64.b64decode(encrypted_text)
+        #seperate the IV from the actual "data"
         iv, ciphertext = encrypted_data[:16], encrypted_data[16:]
 
         cipher = AES.new(key, AES.MODE_CBC, iv)
+        #decrypt the ciphertext and use .decode() to turn it back into a readable string
         decrypted_padded = cipher.decrypt(ciphertext).decode()
 
         padding_length = ord(decrypted_padded[-1])
         return decrypted_padded[:-padding_length]
 
-    def hash_sha3_256(self, message):
+    def sha3(self, message):
+        #hashes the input and returns the message digest
         hasher = SHA3_256.new(message.encode())
         return hasher.hexdigest()
 
@@ -47,5 +54,5 @@ if __name__ == "__main__":
     print("AES Decrypted:", decrypted_text)
 
     # Hashing Examples
-    hashed_sha3_256 = crypto.hash_sha3_256(plaintext)
-    print("SHA3-256 Hash:", hashed_sha3_256)
+    hashed_sha3 = crypto.sha3(plaintext)
+    print("SHA3-256 Hash:", hashed_sha3)
